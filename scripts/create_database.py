496e0e3b5ade5db799fc63cbe84cc3d97e1f85ee
au168-52 <aungemuth@gmail.com>
2025-10-16 09:15:52 +0200

    creation d'une table

--- scripts/create_database.py
+++ scripts/create_database.py
@@ -1,1 +1,31 @@
-import numpy as np
+#Creation base de donnee
+
+import mysql.connector
+	db_connection = mysql.connector.connect(
+  	host="localhost",
+  	user="root",
+  	passwd="password"
+    )
+
+
+
+# creating database_cursor to perform SQL operation
+db_cursor = db_connection.cursor()
+# executing cursor with execute method and pass SQL query
+db_cursor.execute("CREATE DATABASE Inventaire PJT")
+
+db_cursor.execute("CREATE TABLE Materiel(id_materiel INT PRIMARY KEY, date_garantie TEXT, date_dernier_entretient TEXT, derniere_localisation TEXT) ")
+
+
+
+
+
+
+
+
+
+
+#get database table
+for table in db_cursor:
+	print(table)
+    
