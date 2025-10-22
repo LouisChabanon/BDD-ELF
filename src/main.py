0f7603c4c76807c65d04f314351d64bbe105e12f
LouisChabanon <louis@chabanon.es>
2025-10-22 23:56:43 +0200

    -added .venv to .gititgnore - added requirements.txt - added theme file

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 import customtkinter as ctk
 from app_controller import AppController
-
+from database.connection import init_db
 
 if __name__ == "__main__":
     ctk.set_appearance_mode("System")
-    ctk.set_default_color_theme("blue")
+    ctk.set_default_color_theme("src/assets/theme.json")
     
+    try:
+        init_db()
+        print("Connexion à la base de données réussie.")
+    except Exception as e:
+        print(f"Erreur de connexion à la base de données : {e}")
+        exit(1)
+
+
     app = AppController()
     app.mainloop()
