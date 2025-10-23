2efedf49677bb0b0e6a00ad6ccfa35a18b515b36
chloedufau20-hash <chloedufau20@gmail.com>
2025-10-23 09:40:08 +0200

    Product page

--- src/app_controller.py
+++ src/app_controller.py
@@ -2,6 +2,7 @@ import customtkinter as ctk
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 from pages.register_page import RegisterPage
+from pages.product_page import ProductPage
 
 class AppController(ctk.CTk):
     def __init__(self):
@@ -20,9 +21,7 @@ class AppController(ctk.CTk):
         self.show_page("LoginPage")
 
     def _init_pages(self):
-        self.pages["LoginPage"] = LoginPage(self.container, self)
-        self.pages["MainPage"] = MainPage(self.container, self)
-        self.pages["RegisterPage"] = RegisterPage(self.container, self)
+        self.pages["ProductPage"] = ProductPage(self.container, self)
 
         for page in self.pages.values():
             page.place(relx=0, rely=0, relwidth=1, relheight=1)
