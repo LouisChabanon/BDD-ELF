845f4242f3868900576bbf1bcccc2e01cb78ba7d
LouisChabanon <louis@chabanon.es>
2025-10-24 14:35:04 +0200

    fix creation bdd

--- scripts/create_database.py
+++ scripts/create_database.py
@@ -39,9 +39,9 @@ db_connection = mysql.connector.connect(
 
 #Implémentation des tables
 db_cursor = db_connection.cursor()
-db_cursor.execute("CREATE TABLE Materiel(id_materiel INT PRIMARY KEY, date_garantie TEXT, date_dernier_entretient TEXT, derniere_localisation TEXT) ")
 db_cursor.execute("CREATE TABLE Emprunt(id_emprunt INT PRIMARY KEY, motif TEXT, date_emprunt TEXT)")
 db_cursor.execute("CREATE TABLE Matos(nom_materiel VARCHAR(100) PRIMARY KEY, photo_materiel TEXT, frequence_entretient TEXT)")
+db_cursor.execute("CREATE TABLE Materiel(id_materiel INT PRIMARY KEY, date_garantie TEXT, date_dernier_entretient TEXT, derniere_localisation TEXT, nom_materiel VARCHAR(100), FOREIGN KEY (nom_materiel) REFERENCES Matos(nom_materiel)) ")
 db_cursor.execute("CREATE TABLE Rangement(lieu_rangement VARCHAR(100) PRIMARY KEY)")
 db_cursor.execute("CREATE TABLE Kit(nom_kit VARCHAR(100) PRIMARY KEY)")
 db_cursor.execute("CREATE TABLE Historique(date_rendu VARCHAR(100) PRIMARY KEY, id_materiel INT, FOREIGN KEY (id_materiel) REFERENCES Materiel(id_materiel))")
