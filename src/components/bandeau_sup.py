476de82dd4c514dbd749ecbcc96edebdad0a55be
au168-52 <aungemuth@gmail.com>
2026-03-09 14:16:51 +0100

    Au fait ça marchait bien

--- src/components/bandeau_sup.py
+++ src/components/bandeau_sup.py
@@ -18,7 +18,7 @@ class Band_sup(ctk.CTkFrame):
         self.user_button = None
 
         # Bouton Accueil
-        self.home_button = ctk.CTkButton(self,text="🏠",width=20,height=20,fg_color="#B17457",hover_color="#9C6049",font=("Helvetica", 20),command=self.main_page)
+        self.home_button = ctk.CTkButton(self,text="🏠",width=20,height=20,fg_color="#B17457",hover_color="#9C6049",font=("Helvetica", 20),command=lambda: controller.show_page("MainPage"))
 
 
         # Bouton "Ajouter objet"
